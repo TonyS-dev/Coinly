5402f652ed7c71b0a50a12b91d85011052dc5dce
tonys-dev <santiagor.acarlos@gmail.com>
2025-11-18 01:14:53 -0500

    initialize project structure with Gradle, add application properties, and configure .gitignore

---
+++ src/main/kotlin/com/coinly/coinly/CoinlyApplication.kt
@@ -0,0 +1,11 @@
+package com.coinly.coinly
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoinlyApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoinlyApplication>(*args)
+}
---
+++ src/test/kotlin/com/coinly/coinly/CoinlyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coinly.coinly
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoinlyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
